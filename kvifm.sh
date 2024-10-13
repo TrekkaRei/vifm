9a0604913aad690f42104b17e076270510f1c3cb
TrekkaRei <TrekkaRei@gmail.com>
2024-10-13 22:37:18 +0200

    update

--- kvifm.sh
+++
@@ -1,3 +0,0 @@
-konsole -e vifm ~/
-xprop -name ' ' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
-
---
+++ vifm.sh
@@ -0,0 +1,3 @@
+gnome-terminal --geometry=148x33+610+380 -t "VIFM" -- vifm ~
+xprop -name 'VIFM' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
+
