852562479d7fe11889917b9469c5458a5b354e00
TrekkaRei <TrekkaRei@gmail.com>
2024-10-13 20:52:18 +0200

    update

--- kvifm.sh
+++ kvifm.sh
@@ -1,2 +1,3 @@
-konsole -e vifm ~/ && sleep 0.5 && xprop -name '~ - VIFM' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
+konsole -e vifm ~/
+xprop -name ' ' -f _MOTIF_WM_HINTS 32c -set _MOTIF_WM_HINTS "0x2, 0x0, 0x2, 0x0, 0x0"
 
